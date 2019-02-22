8e86b29c87e48cc7c66f6d8559221e3fbd1eab2e
vauxhall <zero.griffin@gmail.com>
2019-02-22 14:58:36 +0100

    Initial Commit

---
+++ TCR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TCR
+//
+//  Created by vin on 22.02.19.
+//  Copyright © 2019 TCR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
